aa89a92588f386d611d4f94fb616d33f64d89bcc
Max Rovensky <hey@fivepointseven.com>
2019-06-10 16:56:50 +0800

    Fix repeated SHA names handling and readyState polling

--- src/deployment-status.ts
+++ src/deployment-status.ts
@@ -4,7 +4,8 @@ import { isDone } from "./utils/ready-state"
 interface DeploymentStatus {
   type: string;
   payload: Deployment | DeploymentBuild[];
-} 
+}
+
 /* eslint-disable */
 export default async function* checkDeploymentStatus(deployment: Deployment, token: string, teamId?: string): AsyncIterableIterator<DeploymentStatus> {
   let deploymentState = deployment
@@ -23,33 +24,34 @@ export default async function* checkDeploymentStatus(deployment: Deployment, tok
 
     for (const build of builds) {
       const prevState = buildsState[build.id]
-  
+
       if (!prevState || prevState.readyState !== build.readyState) {
         yield { type: 'build-state-changed', payload: build }
       }
-  
+
       buildsState[build.id] = build
     }
 
     const readyBuilds = builds.filter((b: DeploymentBuild) => isDone(b))
-    
+
     if (readyBuilds.length === builds.length) {
       allBuildsCompleted = true
       yield { type: 'all-builds-completed', payload: readyBuilds }
     }
   }
-  
+
   // Deployment polling
   const deploymentData = await fetch(`${API_DEPLOYMENTS}/${deployment.id}${teamId ? `?teamId=${teamId}` : ''}`, token)
   const deploymentUpdate = await deploymentData.json()
-  
+
   // Fire deployment state change listeners if needed
   if (deploymentUpdate.readyState !== deploymentState.readyState) {
     if (isDone(deploymentUpdate)) {
-      yield { type: 'ready', payload: deploymentUpdate }
-    } else {
-      deploymentState = deploymentUpdate
-      yield { type: 'deployment-state-changed', payload: deploymentUpdate }
+      return yield { type: 'ready', payload: deploymentUpdate }
     }
+  } else {
+    deploymentState = deploymentUpdate
+
+    yield { type: 'deployment-state-changed', payload: deploymentUpdate }
   }
 }
